5ad23c7f22cf145f3f37ed342d713f63c6edec06
imanfani <iman.farsi.hacker@gmail.com>
2016-03-21 19:27:42 +0430

    Added files via upload

--- plugins/info.lua
+++ plugins/info.lua
@@ -1,12 +1,32 @@
-function run(msg, matches)
-  return "#group id : "..msg.from.id.."\n#group name : "..msg.to.title.."\n#full name : "..(msg.from.first_name or '').."\n#first name : "..(msg.from.first_name or '').."\n#last name : "..(msg.from.last_name or '').."\n#id : "..msg.from.id.."\n#username : @"..(msg.from.username or '').."\n#phone number : +"..(msg.from.phone or '').."\n>info by iman :|
+local function pre_process(msg)
+local to = msg.to.type
+local service = msg.service
+	if to == 'user' and msg.fwd_from then
+		if not is_support(msg.from.id) and not is_admin1(msg) then
+			return
+		end
+		local user = 'user#id'..msg.from.id
+		local from_id = msg.fwd_from.peer_id
+		if msg.fwd_from.first_name then
+			from_first_name = msg.fwd_from.first_name:gsub("_", " ")
+		else
+		end
+		if msg.fwd_from.last_name then
+			from_last_name = msg.fwd_from.last_name:gsub("_", " ")
+		else
+		end
+		if msg.fwd_from.username then
+			from_username = "@"..msg.fwd_from.username
+		else
+		end
+	end
+	return msg
 end
+
+
 return {
-  description = "", 
-  usage = "",
-  patterns = {
-    "^[!/#]id$",
-  },
-  run = run
+    patterns = {
+    },
+    run = run,
+	pre_process = pre_process
 }
-end
