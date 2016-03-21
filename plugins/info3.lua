753564423370ff8eeb2681193f47dda511b188fd
imanfani <iman.farsi.hacker@gmail.com>
2016-03-21 22:53:56 +0430

    Update and rename ID-@BehroozYaghi.lua to info3.lua

---
+++ plugins/info3.lua
@@ -0,0 +1,13 @@
+do
+function run(msg, matches)
+  return "#group id : "..msg.from.id.."\n#group name : "..msg.to.title.."\n#your name : "..(msg.from.first_name or '').."\n#first name : "..(msg.from.first_name or '').."\n#last name : "..(msg.from.last_name or '').."\n#id : "..msg.from.id.."\n#username : @"..(msg.from.username or '').."\n#phone number : +"..(msg.from.phone or '')
+end
+return {
+  description = "", 
+  usage = "",
+  patterns = {
+    "^[!/#]id$",
+  },
+  run = run
+}
+end
