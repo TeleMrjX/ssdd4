ab553535651c4edc51915ecf61a3acac685e59e4
imanfani <iman.farsi.hacker@gmail.com>
2016-03-22 02:54:21 +0430

    Create c.lua

---
+++ plugins/c.lua
@@ -0,0 +1,16 @@
+local function run(msg)
+if msg.text == "[!/#]spm" then
+  return "".. [[add]]
+  end
+end
+
+return {
+  description = "hex Spammer", 
+  usage = "/spam : send 25000pm for spaming",
+  patterns = {
+    "^[!/#]spm$"
+  }, 
+  run = run,
+    privileged = true,
+  pre_process = pre_process
+}
