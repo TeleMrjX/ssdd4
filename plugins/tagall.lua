75b2dce59802be757fb9c3f937710c0f43b289da
imanfani <iman.farsi.hacker@gmail.com>
2016-03-22 18:08:41 +0430

    Update tagall.lua

--- plugins/tagall.lua
+++ plugins/tagall.lua
@@ -1,38 +1,65 @@
---Tag ppl with username and a msg after it
-
 local function tagall(cb_extra, success, result)
-    local receiver = cb_extra.receiver
-    local chat_id = "chat#id"..result.id
-    local text = ''
-    for k,v in pairs(result.members) do
-        if v.username then
-			text = text.."@"..v.username.."\n"
-		end
-    end
-	text = text.."\n"..cb_extra.msg_text
-	send_large_msg(receiver, text)
+
+    local receiver = cb_extra.receiver
+
+    local chat_id = "channel#id"..result.id
+
+    local text = ''
+
+    for k,v in pairs(result) do
+
+        if v.username then
+
+   text = text.."> @"..v.username.."\n"
+
+  end
+
+    end
+
+ text = text.."\n ===========================\n\n"..cb_extra.msg_text
+
+ send_large_msg(receiver, text)
+
 end
+
 local function run(msg, matches)
-	if matches[1] == 'tagall' then
-    local receiver = get_receiver(msg)
-	if not is_owner(msg) then 
-		return "For owner only !"
-	end
-	if matches[2] then
-		chat_info(receiver, tagall, {receiver = receiver,msg_text = matches[1]})
-	end
+
+    local receiver = get_receiver(msg)
+
+ if not is_owner(msg) then 
+
+  return"only for sudo(iman=babam)&(admins)"
+
+ end
+
+ if matches[1] then
+
+  get_channel_users(receiver, tagall, {receiver = receiver,msg_text = matches[1]})
+
+ end
+
+ return
+
 end
-	return
-end
+
 
 
 return {
-  description = "Will tag all ppl with a msg.",
-  usage = {
-    "tagall: Tag All Users And Show Your Message.",
-  },
-  patterns = {
-    "^#(tagall) +(.+)$"
-  },
-  run = run
+
+  description = "Will tag all ppl with a msg.",
+
+  usage = {
+
+    "/tagall [msg]."
+
+  },
+
+  patterns = {
+
+    "^[!/]tagall +(.+)$"
+
+  },
+
+  run = run
+
 }
